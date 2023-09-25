7338b39eecf51a1806e25d542b505b70e33665c0
WMBLACKOUT <141074052+WMBLACKOUT@users.noreply.github.com>
2023-09-25 04:02:05 -0300

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -1,86 +1,129 @@
-// Lista de elementos de áudio e botões de controle
-const audioPlayers = document.querySelectorAll(".audio-player");
-const playPauseButtons = document.querySelectorAll(".play-pause-button");
-const previousButtons = document.querySelectorAll(".previous-button");
-const nextButtons = document.querySelectorAll(".next-button");
-const currentTimes = document.querySelectorAll(".current-time");
-const totalTimes = document.querySelectorAll(".total-time");
+const musicElements = document.querySelectorAll('.music');
+const miniPlayerContainer = document.querySelector('.mini-player-container');
+const miniPlayer = document.querySelector('.mini-player');
+const musicInfo = document.querySelector('.music-info');
+const miniPlayerControls = document.querySelector('.mini-player-controls');
+const playPauseButton = document.querySelector('.play-pause-button');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const replayButton = document.querySelector('.replay-button');
+const progressBar = document.querySelector('.progress-bar');
+const timeSlider = document.querySelector('.progress-bar');
+const currentTimeDisplay = document.querySelector('.current-time');
+const totalTimeDisplay = document.querySelector('.total-time');
+const audio = new Audio();
 
-// Função para pausar todos os áudios
-function pauseAllAudio() {
-    audioPlayers.forEach((audio) => {
-        audio.pause();
-    });
+let currentMusicIndex = 0;
+let isPlaying = false;
+
+function updateMiniPlayer(index) {
+    const music = musicElements[index];
+    const title = music.querySelector('h3').textContent;
+    const artist = music.querySelector('p').textContent;
+    const image = music.querySelector('img').src;
+
+    musicInfo.querySelector('h3').textContent = title;
+    musicInfo.querySelector('p').textContent = artist;
+    miniPlayer.querySelector('img').src = image;
 }
 
-// Função para reproduzir ou pausar o áudio
-function togglePlayPause(audio, playPauseButton, currentIndex) {
-    if (audio.paused) {
-        pauseAllAudio(); // Pausa todos os outros áudios
-        audio.play();
-        playPauseButton.classList.remove("fa-play");
-        playPauseButton.classList.add("fa-pause");
+function playMusic(index) {
+    const music = musicElements[index];
+    const audioSource = music.querySelector('audio').src;
+
+    audio.src = audioSource;
+    audio.currentTime = 0;
+    audio.play();
+    isPlaying = true;
+    updateMiniPlayer(index);
+    playPauseButton.textContent = 'Pause';
+}
+
+function pauseMusic() {
+    audio.pause();
+    isPlaying = false;
+    playPauseButton.textContent = 'Play';
+}
+
+function replayMusic() {
+    audio.currentTime = 0;
+    audio.play();
+    isPlaying = true;
+    playPauseButton.textContent = 'Pause';
+}
+
+function nextMusic() {
+    currentMusicIndex = (currentMusicIndex + 1) % musicElements.length;
+    playMusic(currentMusicIndex);
+}
+
+function prevMusic() {
+    currentMusicIndex = (currentMusicIndex - 1 + musicElements.length) % musicElements.length;
+    playMusic(currentMusicIndex);
+}
+
+function updateSlider() {
+    const currentTime = audio.currentTime;
+    const duration = audio.duration;
+
+    timeSlider.value = (currentTime / duration) * 100;
+    progressBar.value = (currentTime / duration) * 100;
+    currentTimeDisplay.textContent = formatTime(currentTime);
+    totalTimeDisplay.textContent = formatTime(duration);
+}
+
+function formatTime(timeInSeconds) {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = Math.floor(timeInSeconds % 60);
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+}
+
+playPauseButton.addEventListener('click', () => {
+    if (isPlaying) {
+        pauseMusic();
     } else {
-        audio.pause();
-        playPauseButton.classList.remove("fa-pause");
-        playPauseButton.classList.add("fa-play");
+        playMusic(currentMusicIndex);
     }
-}
+});
 
-// Função para retroceder para a faixa anterior
-function playPrevious(audio, currentIndex) {
-    const previousIndex = currentIndex - 1;
+replayButton.addEventListener('click', () => {
+    replayMusic();
+});
 
-    if (previousIndex >= 0) {
-        pauseAllAudio(); // Pausa todos os outros áudios
-        audioPlayers[previousIndex].currentTime = 0;
-        audioPlayers[previousIndex].play();
-    }
-}
+prevButton.addEventListener('click', () => {
+    prevMusic();
+});
 
-// Função para avançar para a próxima faixa
-function playNext(audio, currentIndex) {
-    const nextIndex = currentIndex + 1;
+nextButton.addEventListener('click', () => {
+    nextMusic();
+});
 
-    if (nextIndex < audioPlayers.length) {
-        pauseAllAudio(); // Pausa todos os outros áudios
-        audioPlayers[nextIndex].currentTime = 0;
-        audioPlayers[nextIndex].play();
-    }
-}
+timeSlider.addEventListener('input', () => {
+    const duration = audio.duration;
+    const seekTime = (timeSlider.value / 100) * duration;
+    audio.currentTime = seekTime;
+});
 
-// Atualiza o tempo atual e o tempo total do áudio
-function updateAudioTime(audio, currentIndex) {
-    const currentTime = currentTimes[currentIndex];
-    const totalTime = totalTimes[currentIndex];
+progressBar.addEventListener('input', () => {
+    const duration = audio.duration;
+    const seekTime = (progressBar.value / 100) * duration;
+    audio.currentTime = seekTime;
+});
 
-    audio.addEventListener("timeupdate", () => {
-        const currentMinutes = Math.floor(audio.currentTime / 60);
-        const currentSeconds = Math.floor(audio.currentTime % 60);
-        const totalMinutes = Math.floor(audio.duration / 60);
-        const totalSeconds = Math.floor(audio.duration % 60);
-
-        currentTime.textContent = `${currentMinutes}:${currentSeconds < 10 ? "0" : ""}${currentSeconds}`;
-        totalTime.textContent = `${totalMinutes}:${totalSeconds}`;
-    });
-}
-
-// Adicionar eventos de clique aos botões de controle
-playPauseButtons.forEach((button, index) => {
-    button.addEventListener("click", () => {
-        togglePlayPause(audioPlayers[index], button, index);
-        updateAudioTime(audioPlayers[index], index);
+musicElements.forEach((music, index) => {
+    music.addEventListener('click', () => {
+        currentMusicIndex = index;
+        playMusic(currentMusicIndex);
     });
 });
 
-previousButtons.forEach((button, index) => {
-    button.addEventListener("click", () => {
-        playPrevious(audioPlayers[index], index);
-    });
+audio.addEventListener('ended', () => {
+    nextMusic();
 });
 
-nextButtons.forEach((button, index) => {
-    button.addEventListener("click", () => {
-        playNext(audioPlayers[index], index);
-    });
+audio.addEventListener('timeupdate', () => {
+    updateSlider();
 });
+
+
+playMusic(currentMusicIndex);
